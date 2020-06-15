8a1e5c61fe219346de9c4555ddf5fa7b2aa287fd
Xueyu <35753823+zerosunyata@users.noreply.github.com>
2020-06-15 10:04:24 -0400

    Update test.java

--- test.java
+++ test.java
@@ -5,12 +5,12 @@ import java.util.*;
 public class test {
 	public static void main(String[] args) throws Exception{
 		Robot hal = new Robot();
-		Random random = new Random();
+		//Random random = new Random();
 		while(true){
-			hal.delay(1000*60);
-			int x = random.nextInt()%640;
-			int y = random.nextInt()%480;
-			hal.mouseMove(x,y);
+			hal.delay(100*60);
+			double x = (MouseInfo.getPointerInfo().getLocation().getX() + 1)%2360;
+			double y = (MouseInfo.getPointerInfo().getLocation().getY() + 1)%1920;
+			hal.mouseMove((int)x,(int)y);
 		}
 	}
 	
